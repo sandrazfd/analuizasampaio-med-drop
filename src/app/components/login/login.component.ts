b33e2465ea0051f72ae3a62849a17d990fb023aa
sandrazfd <sandrazfd@hotmail.com>
2023-05-26 15:55:01 -0300

    initial commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { UserService } from './../../services/user.service';
-import { User } from './../../classe/user';
+
+
 
 @Component({
   selector: 'app-login',
@@ -10,29 +10,13 @@ import { User } from './../../classe/user';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router : Router, private userService : UserService ){}
+  constructor(){}
 
-  user! : User;
-  erro!: string;
-  storage : Storage = localStorage;
+  
 
   ngOnInit(): void {
-    this.user = new User();
-    this.storage.removeItem('user_name')
+    
   }
-  fechar(): void{
-    this.router.navigate(['/CADASTRO'])// precisa fazer a definição desta rota
-  }
-  validar(user: User) :void{
-
-    this.userService.postUsuarioApi(user).subscribe(res=> {
-      console.log(res)
-      if(res){
-        this.storage.setItem('user_name', user.username) 
-        this.fechar()
-      }else{
-        this.erro = "Usuário ou senha inválidos";
-      }
-    })
-  }
+  
+  
 }
